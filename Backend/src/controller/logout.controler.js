1f49e260b5e5a152677e8751382ef61d72509987
papai <Papai Pc@Lenovo.com>
2024-02-04 16:44:06 +0530

    code

---
+++ Backend/src/controller/logout.controler.js
@@ -0,0 +1,12 @@
+import { Employe } from "../model/empolye.model"
+
+ export const logout= async (req,res)=>{
+ try {
+     const {email}= req.query.email
+     const user= await Employe
+
+    
+ } catch (error) {
+    
+ }
+}
